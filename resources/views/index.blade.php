2f2cee73ae704ee1d12775761dcbf00f73fd3635
JFN-N <ec2-user@ip-172-31-11-16.ap-northeast-1.compute.internal>
2023-07-02 11:54:17 +0000

    任意のメッセージ

---
+++ resources/views/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layout1')
+
+{{-- メインコンテンツ --}}
+@section('contets1')
+
+        <h1>ログイン</h1>
+        @if ($errors->any())
+            <div>
+            @foreach ($errors->all() as $error)
+                {{ $error }}<br>
+            @endforeach
+            </div>
+        @endif
+        @if (session('front.user_register_success') == true)
+                ユーザーを登録しました！！<br>
+        @endif
+        <form action="/login" method="post">
+            @csrf
+            email：<input name="email"><br>
+            パスワード：<input name="password" type="password"><br>
+            <button>ログインする</button>
+        </form>
+
+        <a href="/user/register">会員登録</a><br>
+
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/', [AuthController::class, 'index'])->name('front.index');
+Route::post('/login', [AuthController::class, 'login']);
