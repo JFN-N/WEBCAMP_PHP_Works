5bd430c6d548f448f218a1d83a2f2da474775934
JFN-N <ec2-user@ip-172-31-11-16.ap-northeast-1.compute.internal>
2023-07-24 14:38:51 +0000

    今現在

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -3,11 +3,14 @@
 {{-- メインコンテンツ --}}
 @section('contets')
     <div class=main>
+
+
         <div class="header"></div>
-    <div class=main-content>
-        <div class=top>
-        <h1>レシピマスター ログイン</h1>
-        </div>
+        <div class="header-color"></div>
+        <div class=main-content>
+            <div class=top>
+            <h1>レシピマスター ログイン</h1>
+            </div>
 
         @if ($errors->any())
             <div>
@@ -20,18 +23,18 @@
                 ユーザーを登録しました！！<br>
         @endif
 
-        <div class=form-main>
-        <form action="/login" method="post">
-            @csrf
-            email：<input name="email"><br>
-            パスワード：<input name="password" type="password"><br>
-            <button>ログインする</button>
-        </form>
-        </div>
+            <div class=form-main>
+            <form action="/login" method="post">
+                @csrf
+                email：<input name="email"><br>
+                パスワード：<input name="password" type="password"><br>
+                <button>ログインする</button>
+            </form>
+            </div>
 
-        <div class=register>
-        <a href="/user/register">会員登録</a>
-        </div>
+            <div class=register>
+            <a href="/user/register">会員登録</a>
+            </div>
 
     </div>
 @endsection
