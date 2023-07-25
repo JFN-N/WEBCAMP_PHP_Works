85a142b9c4529239eef528ed4175a6bd41aa97b9
JFN-N <ec2-user@ip-172-31-11-16.ap-northeast-1.compute.internal>
2023-07-25 13:28:52 +0000

    今現在

--- resources/views/main/menu.blade.php
+++ resources/views/main/menu.blade.php
@@ -3,11 +3,14 @@
 {{-- メインコンテンツ --}}
 @section('main.contets')
 <div class = section>
-        <div class = section-text>
+    <div class=header>
+    <div clas=header-center></div>
+    </div>
+    <div class = section-text>
         <h1>メニュー</h1>
          <a href="/archive/data">証明書</a><br>
         <a href="/recipe/list">登録</a><br>
         <a href="/logout">ログアウト</a>
-        </div>
+    </div>
 </div>
 @endsection
