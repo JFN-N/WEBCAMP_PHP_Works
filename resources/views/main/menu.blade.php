4f1312b7944d971c9025eb01ac01df3d2ac426c5
JFN-N <ec2-user@ip-172-31-11-16.ap-northeast-1.compute.internal>
2023-08-22 12:57:47 +0000

    ちょっと変更した

--- resources/views/main/menu.blade.php
+++ resources/views/main/menu.blade.php
@@ -2,13 +2,31 @@
 
 {{-- メインコンテンツ --}}
 @section('main.contets')
-<div class = section>
-    <div class=header></div>
-    <div class = section-text>
-        <h1>レシピマスター|メニュー</h1>
-         <a href="/archive/data">証明書</a><br>
-        <a href="/recipe/list">登録</a><br>
-        <a href="/logout">ログアウト</a>
+<div class="header">
+    <div class ="header-logo">
+        <h1>レシピマスター</h1>
     </div>
 </div>
+
+<div class="body">
+
+    <div class="archive-button">
+        <a href="/archive/data">
+            <button type="button">証明書</button>
+        </a><br>
+    </div>
+
+    <div class="list-button">
+        <a href="/recipe/list">登録</a>
+    </div>
+
+</div>
+
+
+
+
+<div class="footer">
+    <a href="/logout">ログアウト</a>
+</div>
+
 @endsection
