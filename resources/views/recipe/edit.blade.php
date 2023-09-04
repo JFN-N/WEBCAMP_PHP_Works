ffe81b1cf3952ad676e640611205db2401a4d8dd
JFN-N <staiju77@gmail.com>
2023-09-04 12:46:06 +0000

    ちょっと変更した

--- resources/views/recipe/edit.blade.php
+++ resources/views/recipe/edit.blade.php
@@ -26,19 +26,18 @@
             <form action="{{ route('edit_save', ['recipe_id' => $recipe->id]) }}" method="post">
                 @csrf
                 @method("PUT")
-                <div class="name">料理名:<input name="name" value="{{ old('name') ?? $recipe->name }}"></div><br>
+                <div class="name">料理名:<input name="name" value="{{ old('name') ?? $recipe->name }}" class="name-text"></div>
 
                 <div class="type">
-                種類:<label><input type="radio" name="type" value="1" @if ((old('type') ?? $recipe->type) == 1) checked @endif>肉</label> /
-                    <label><input type="radio" name="type" value="2" @if ((old('type') ?? $recipe->type) == 2) checked @endif>魚</label> /
-                    <label><input type="radio" name="type" value="3" @if ((old('type') ?? $recipe->type) == 3) checked @endif>野菜</label> /
-                    <label><input type="radio" name="type" value="4" @if ((old('type') ?? $recipe->type) == 4) checked @endif>その他</label>
+                種類:&nbsp;<label><input type="radio" name="type" value="1" @if ((old('type') ?? $recipe->type) == 1) checked @endif>&nbsp;肉&nbsp;&nbsp;</label>
+                    <label><input type="radio" name="type" value="2" @if ((old('type') ?? $recipe->type) == 2) checked @endif>&nbsp;魚&nbsp;&nbsp;</label>
+                    <label><input type="radio" name="type" value="3" @if ((old('type') ?? $recipe->type) == 3) checked @endif>&nbsp;野菜&nbsp;&nbsp;</label>
+                    <label><input type="radio" name="type" value="4" @if ((old('type') ?? $recipe->type) == 4) checked @endif>&nbsp;その他</label>
                     </div>
-                    <br>
 
-                <div class="detail">料理の詳細:<br><textarea name="detail">{{ old('detail') ?? $recipe->detail }}</textarea></div><br>
+                <div class="detail">料理の詳細:<br><textarea name="detail">{{ old('detail') ?? $recipe->detail }}</textarea></div>
 
-                <div class="edit-btn"><button>編集完了</button></div>
+                <div class="edit-btn"><button class="btn01">編集完了</button></div>
 
             </form>
         </div>
@@ -47,7 +46,8 @@
         <hr>
         <div class="footer">
         <menu label="リンク">
-        <a href="/recipe/list">料理の登録・一覧に戻る</a><br>
+        <a href="/main/menu">メインメニューに戻る</a><br><br>
+        <a href="/recipe/list">料理の登録・確認に戻る</a><br>
         <br><a href="/logout">ログアウト</a><br>
         </menu>
         </div>
