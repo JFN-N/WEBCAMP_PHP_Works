96bc93399ff738b5496ef25e85740f6a754b8759
JFN-N <staiju77@gmail.com>
2023-09-05 11:53:25 +0000

    ちょっと変更した

--- resources/views/recipe/completed_list.blade.php
+++ resources/views/recipe/completed_list.blade.php
@@ -34,22 +34,40 @@
         <!-- ページネーション -->
         {{-- {{ $list->links() }} --}}
         現在 {{ $list->currentPage() }} ページ目<br>
+
+        <!--
         @if ($list->onFirstPage() === false)
             <a href="/recipe/completed_list">最初のページ</a>
         @else
             最初のページ
         @endif
-        /
+        -->
+        @if ($list->previousPageUrl() !== null)
+            <a href="{{ $list->previousPageUrl() }}">＜</a>
+        @else
+            &nbsp;
+        @endif
+
+        |
+        <!--
         @if ($list->previousPageUrl() !== null)
             <a href="{{ $list->previousPageUrl() }}">前に戻る</a>
         @else
             前に戻る
         @endif
-        /
-        @if ($list->nextPageUrl() !== null)
-            <a href="{{ $list->nextPageUrl() }}">次に進む</a>
+        -->
+
+        @if ($list->onFirstPage() === false)
+            <a href="/recipe/completed_list">&nbsp;最初のページ&nbsp;</a>
         @else
-            次に進む
+            &nbsp;
+        @endif
+
+        |
+        @if ($list->nextPageUrl() !== null)
+            <a href="{{ $list->nextPageUrl() }}">≫</a>
+        @else
+            &nbsp;
         @endif
         </div>
 
