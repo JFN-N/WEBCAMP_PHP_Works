9456cea35c0e6a001ddc2a9588d7945f764f9c97
JFN-N <staiju77@gmail.com>
2023-09-30 14:16:15 +0000

    added

--- resources/views/recipe/detail.blade.php
+++ resources/views/recipe/detail.blade.php
@@ -19,11 +19,7 @@
         </div>
     </div>
 
-    <div class="alert">
-        @if (session('front.task_edit_success') == true)
-        レシピを編集しました！！<br>
-        @endif
-    </div>
+    <div class="main">
 
     <div class="contents">
         <div class="name">料理名： {{ $recipe->name }}<br></div>
@@ -31,10 +27,12 @@
         <div class="detail">料理の詳細： <pre>{{ $recipe->detail }}</pre><br></div>
     </div>
 
-        <hr>
+    </div>
+
+
+
         <div class="footer">
         <a href="/main/menu">メインメニューに戻る</a>
         <br><br><a href="/recipe/list">料理の登録・確認に戻る</a>
-        <br><br><a href="/logout">ログアウト</a>
         </div>
 @endsection
