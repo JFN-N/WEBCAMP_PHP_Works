dd753c614ba8d74f903f4515d1599ac720bda33f
JFN-N <staiju77@gmail.com>
2023-10-03 13:59:41 +0000

    add

--- resources/views/user/register.blade.php
+++ resources/views/user/register.blade.php
@@ -4,11 +4,9 @@
 @section('major.contets')
     <div class="header">
         <div class="header-logo">
-        <h1>レシピマスター</h1>
+        <h1><i class="fa-solid fa-pizza-slice"></i>&nbsp;レシピマスター</h1>
         </div>
     </div>
-
-    <hr>
 
     <div class=main>
         <div class="main-header">
